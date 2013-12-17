8b209708218307e8b0a9eb235cc261c74ae50409
RaivU <ravi.undupitiya@gmail.com>
2013-12-17 11:49:34 +0530

    Adding some script header tags

--- main.py
+++ main.py
@@ -22,7 +22,8 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__version__ = '0.0.1'
+__version__     = '0.0.1'
+__author__      = 'RaviU'
 
 import os
 import sys
--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ repo_location = '/home/ravi/WORK/RD/carbon/platform/branches/turing/'
 # Run order of modules, a tuple containing key value pairs. Key is component name and value is class name
 run_order = (
             'Test',
-            'JarDuplicateChecker'
+            'Test2',
             )
