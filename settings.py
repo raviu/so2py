1ed161a8372c09af5e4e7729f23a8822d799deda
shafreen <shafreen@shaf.(none)>
2013-12-28 21:03:34 +0530

    removing test 1/2 components from settings file

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # Location of platform inside code repository  
-repo_location = ''#UPDATE_REPO_LOCATION
+repo_location = '' #UPDATE_REPO_LOCATION
 
 # nexus details
 nexus_url = 'http://maven.wso2.org/'
@@ -7,8 +7,6 @@ nexus_repo_name = 'wso2-public'
 
 # Run order of modules, a tuple containing list of module class names
 run_order = (
-            'Test',
-            'Test2',
             'Component',
 	    'Feature'
             )
